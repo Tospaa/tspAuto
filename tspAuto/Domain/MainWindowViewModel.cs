82b4de16ee98800c88994e5bca327bf721531178
Tospaa <Tospaa@SEVKET>
2019-03-04 14:38:42 +0300

    ilk yorum

---
+++ tspAuto/Domain/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tspAuto.Domain
+{
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            PanelItems = new[]
+                {
+                    new PanelItem("Ana Sayfa", new Home()),
+                    new PanelItem("Taslak", new taslak())
+                };
+        }
+
+        public PanelItem[] PanelItems { get; }
+    }
+}
