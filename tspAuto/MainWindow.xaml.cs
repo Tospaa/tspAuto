2d076811232f38d35d387089f220a5223a6d5df9
Tospaa <musaecer@gmail.com>
2019-03-19 18:36:50 +0300

    System Tray Icon eklendi.

--- tspAuto/MainWindow.xaml.cs
+++ tspAuto/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -11,11 +13,75 @@ namespace tspAuto
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.ComponentModel.IContainer components;
+
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new MainWindowViewModel();
+
+            components = new System.ComponentModel.Container();
+            contextMenu1 = new System.Windows.Forms.ContextMenu();
+            menuItem1 = new System.Windows.Forms.MenuItem();
+            menuItem2 = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { menuItem1, menuItem2 });
+
+            // Initialize menuItem1
+            menuItem1.Index = 0;
+            menuItem1.Text = "Programı Aç";
+            menuItem1.Click += new EventHandler(notifyIcon1_DoubleClick);
+
+            // Initialize menuItem2
+            menuItem2.Index = 1;
+            menuItem2.Text = "Kapat";
+            menuItem2.Click += new EventHandler(menuItem1_Click);
+
+            // Create the NotifyIcon.
+            notifyIcon1 = new System.Windows.Forms.NotifyIcon(components);
+
+            // The Icon property sets the icon that will appear
+            // in the systray for this application.
+            notifyIcon1.Icon = System.Drawing.Icon.ExtractAssociatedIcon(AppDomain.CurrentDomain.BaseDirectory + @"\tspAuto.exe");
+
+            // The ContextMenu property sets the menu that will
+            // appear when the systray icon is right clicked.
+            notifyIcon1.ContextMenu = contextMenu1;
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+            notifyIcon1.Text = Properties.Resources.Title;
+            notifyIcon1.Visible = true;
+
+            // Handle the DoubleClick event to activate the form.
+            notifyIcon1.DoubleClick += new EventHandler(notifyIcon1_DoubleClick);
+        }
+
+        private void notifyIcon1_DoubleClick(object Sender, EventArgs e)
+        {
+            // Show the form when the user double clicks on the notify icon.
+
+            // Set the WindowState to normal if the form is minimized.
+            if (!IsVisible)
+            {
+                Show();
+            }
+
+            // Activate the form.
+            Activate();
+        }
+
+        private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            // Close the form, which closes the application.
+            reallyClose = true;
+            Close();
         }
 
         //private static Notification notification;
@@ -64,6 +130,7 @@ namespace tspAuto
         {
             if (reallyClose)
             {
+                notifyIcon1.Visible = false;
                 Properties.Settings.Default.SonArama = string.Empty;
                 Properties.Settings.Default.Save();
             }
